6029e48695d12ba5280762798f1b01ff7fcee1b7
Adriano Ferreira <adrianosf@id.uff.br>
2025-01-08 19:39:24 -0300

    feat: end-points
    End points finalizados, porém com bd em memória principal

--- api_anuff/api_anuff/app.py
+++ api_anuff/api_anuff/app.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI
 
 from routers.usuarios import router as usuarios_router
 from routers.anuncios import router as anuncios_router
+from routers.chats import router as chats_router
+from routers.mensagens import router as mensagens_router
+from routers.avaliacoes import router as avaliacoes_router
 
 app = FastAPI()
 
 # Registrar as rotas de usuários
 app.include_router(usuarios_router, prefix="/usuarios", tags=["Usuários"])
 app.include_router(anuncios_router, prefix="/anuncios", tags=["Anúncios"])
+app.include_router(chats_router, prefix="/chats", tags=["Chats"])
+app.include_router(mensagens_router, prefix="/mensagens", tags=["Mensagens"])
+app.include_router(avaliacoes_router, prefix="/avaliacoes", tags=["Avaliações"])
+
+
 
 @app.get("/", response_model=dict)
 def read_root():
